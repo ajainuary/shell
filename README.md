1655347a3fb588ac2ba29d3c9e5dbfd483f327b0
Anurag Jain <ajainuary@gmail.com>
2018-08-30 22:51:31 +0530

    ls command implemented

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ls command should take filename as parameter
 check status of process in process control block
+replace fixed file path size with PATH MAX
+implement expansion of ~
--- prompt.c
+++ prompt.c
@@ -1,3 +1,4 @@
+#include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <getopt.h>
@@ -10,8 +11,8 @@
 #include <sys/utsname.h>
 #include <sys/wait.h>
 #include <syscall.h>
+#include <time.h>
 #include <unistd.h>
-#include <dirent.h>
 #define catch if (errno != 0){ \
     write(1, "Error: ", 7);    \
     write(1, strerror(errno), strlen(strerror(errno))); write(1, "\n", 1); }
@@ -25,11 +26,9 @@ char argcount;
 struct Info {
   int uid;
   char uname[4096];
-  // char home[4096];
 };
 
-void details(struct Info *instance) {
-  instance->uid = getuid();
+void get_user(int uid, char *uname) {
   int list = open("/etc/passwd", O_RDONLY);
   catch;
   char buffer[MAX_READ + 1];
@@ -38,12 +37,12 @@ void details(struct Info *instance) {
   for (char *s = strtok(buffer, "\n"); s != NULL; s = strtok(NULL, "\n")) {
     char *name = strtok(NULL, ":");
     strtok(NULL, ":");  // Cycle through the password
-    char *uid = strtok(NULL, ":");
-    if (uid != NULL && atoi(uid) == instance->uid) {
+    char *this_uid = strtok(NULL, ":");
+    if (this_uid != NULL && atoi(this_uid) == uid) {
       strtok(NULL, ":");  // Cycle through Group ID
       strtok(NULL, ":");  // Cycle through extra info
       // char *home = strtok(NULL, ":");
-      strcpy(instance->uname, name);
+      strcpy(uname, name);
       // strcpy(instance->home, home);
       break;
     }
@@ -56,8 +55,8 @@ void prompt() {
   char prompt_text[16384];
   getcwd(path, 4096);
   catch;
-  struct Info info;
-  details(&info);
+  char name[4096];
+  get_user(getuid(), name);
   struct utsname details;
   uname(&details);
   catch;
@@ -66,7 +65,7 @@ void prompt() {
     start_path = strlen(home) - 1;
     path[start_path] = '~';
   }
-  sprintf(prompt_text, "<%s@%s:%s> ", info.uname, details.nodename,
+  sprintf(prompt_text, "<%s@%s:%s> ", name, details.nodename,
           path + start_path);
   print(prompt_text);
 }
@@ -74,14 +73,12 @@ void prompt() {
 void read_cmd() {
   char buffer[131072];
   int i = 0;
-  while((buffer[i] = fgetc(stdin)) != EOF)
-  {
-    if(buffer[i] == '\n' && buffer[i-1] != '\\')
-      break; //Proposed feature : Multiline Input
+  while ((buffer[i] = fgetc(stdin)) != EOF) {
+    if (buffer[i] == '\n' && buffer[i - 1] != '\\')
+      break;  // Proposed feature : Multiline Input
     ++i;
   }
-  if(i == 0)
-  {
+  if (i == 0) {
     argcount = 0;
     return;
   }
@@ -108,7 +105,7 @@ void pwd() {
 
 void cd() {
   if (arg[1][0] == '~') {
-    chdir(home);
+    chdir(home);  // Handle relative paths
     catch;
   } else {
     chdir(arg[1]);
@@ -116,14 +113,52 @@ void cd() {
   }
 }
 
+void perms(int perm[][3], struct stat *test) {
+  if ((test->st_mode & S_IRUSR) != 0)
+    perm[0][0] = 1;
+  else
+    perm[0][0] = 0;
+  if ((test->st_mode & S_IWUSR) != 0)
+    perm[0][1] = 1;
+  else
+    perm[0][1] = 0;
+  if ((test->st_mode & S_IXUSR) != 0)
+    perm[0][2] = 1;
+  else
+    perm[0][2] = 0;
+  if ((test->st_mode & S_IRGRP) != 0)
+    perm[1][0] = 1;
+  else
+    perm[1][0] = 0;
+  if ((test->st_mode & S_IWGRP) != 0)
+    perm[1][1] = 1;
+  else
+    perm[1][1] = 0;
+  if ((test->st_mode & S_IXGRP) != 0)
+    perm[1][2] = 1;
+  else
+    perm[1][2] = 0;
+  if ((test->st_mode & S_IROTH) != 0)
+    perm[2][0] = 1;
+  else
+    perm[2][0] = 0;
+  if ((test->st_mode & S_IWOTH) != 0)
+    perm[2][1] = 1;
+  else
+    perm[2][1] = 0;
+  if ((test->st_mode & S_IXOTH) != 0)
+    perm[2][2] = 1;
+  else
+    perm[2][2] = 0;
+  return;
+}
+
 void ls() {
   extern char *optarg;
   extern int optind;
   int c, flag_long = 0, flag_hidden = 0, flag_path = 0;
-  while((c = getopt(argcount, arg, "al")) != -1)
-  {
-    switch(c)
-    {
+  while ((c = getopt(argcount, arg, "al")) != -1) {
+    switch (c) {
       case 'l':
         flag_long = 1;
         break;
@@ -135,36 +170,57 @@ void ls() {
         break;
     }
   }
-  if(optind < argcount)
-    flag_path = 1;
-  DIR * p;
-  if(flag_path == 1)
-  {
+  char path[4096];
+  if (optind < argcount) flag_path = 1;
+  DIR *p;
+  if (flag_path == 1) {
     p = opendir(arg[optind]);
     catch;
-  }
-  else
-  {
-    char path[4096];
+  } else {
     getcwd(path, 4096);
     p = opendir(path);
     catch;
   }
-  optind = 1;
-  struct dirent * file;
-  while((file = readdir(p)))
-  {
-    if(file->d_name[0] == '.')
-    {
-      if(flag_hidden == 1)
-      {
-        printf("%s ", file->d_name);
+  struct dirent *file;
+  while ((file = readdir(p))) {
+    int print = 1;
+    if (flag_hidden == 0 && file->d_name[0] == '.') print = 0;
+    if (print == 1) {
+      if (flag_long == 1) {
+        struct stat details;
+        char tmp[4096];
+        strcpy(tmp, (flag_path == 1) ? arg[optind] : path);
+        int len = strlen(tmp);
+        tmp[len] = '/';
+        tmp[len + 1] = '\0';
+        strcat(tmp, file->d_name);
+        lstat(tmp, &details);
+        int perm[3][3];
+        perms(perm, &details);
+        char user[4096];
+        get_user(details.st_uid, user);
+        char group[4096];
+        get_user(details.st_gid, group);
+        char time[256];
+        ctime_r(&details.st_mtime, time);
+        int length = strlen(time);
+        int i;
+        for (i = 0; time[i] != '\n'; ++i);
+        time[i] = '\0';
+        printf("%c%c%c%c%c%c%c%c%c %3lu %12s %12s %10ld %s %s\n",
+               (perm[0][0] == 0) ? '-' : 'r', (perm[0][1] == 0) ? '-' : 'w',
+               (perm[0][2] == 0) ? '-' : 'x', (perm[1][0] == 0) ? '-' : 'r',
+               (perm[1][1] == 0) ? '-' : 'w', (perm[1][2] == 0) ? '-' : 'x',
+               (perm[2][0] == 0) ? '-' : 'r', (perm[2][1] == 0) ? '-' : 'w',
+               (perm[2][2] == 0) ? '-' : 'x', details.st_nlink, user, group,
+               details.st_size, time, file->d_name);
       }
+      else
+        printf("%s ", file->d_name);
     }
-    else
-      printf("%s ", file->d_name);
   }
   printf("\n");
+  optind = 1;
   return;
 }
 
@@ -177,8 +233,7 @@ int main(int argc, char *argv[], char *envp[]) {
   while (run == 0) {
     prompt();
     read_cmd();
-    if(argcount == 0)
-      continue;
+    if (argcount == 0) continue;
     if (strncmp(arg[0], "cd", 2) == 0) {
       cd();
     } else if (strncmp(arg[0], "ls", 2) == 0) {
